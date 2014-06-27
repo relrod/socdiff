645bf5b9b300d9494dca85d6a8ca4afcd5a99b44
Ricky Elrod <ricky@elrod.me>
2014-06-27 06:13:07 -0400

    make the program look like it does something
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- socdiff.hs
+++ socdiff.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Data.List (intercalate)
+import Haxl.Core
+import qualified Web.Socdiff.Github.DataSource as Github
+import qualified Web.Socdiff.Github.Github as Github
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>))
+
 main :: IO ()
-main = putStrLn "hi"
+main = do
+  githubState <- Github.initGlobalState 10
+  ghEnv <- initEnv (stateSet githubState stateEmpty) ()
+  putStrLn "Fetching Github followers"
+  r <- runHaxl ghEnv $ do
+    followers <- Github.getFollowers "CodeBlock" -- TODO: We'll need a config file for this
+    return $ fmap ("Github:"++) followers -- TODO: Can we use access the data source's name?
+  home <- getHomeDirectory
+  let cachePath = home </> ".socdiff_cache"
+  writeFile cachePath ""
+  appendFile cachePath (intercalate "\n" r)
+  appendFile cachePath "\n"
+  putStrLn $ "Stored " ++ cachePath
