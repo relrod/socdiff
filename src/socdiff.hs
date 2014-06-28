d2864fb0d0816272e0a0d6a5eeb5597c15b81018
Ricky Elrod <ricky@elrod.me>
2014-06-28 05:17:58 -0400

    refactor in prep for other data sources
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/socdiff.hs
+++ src/socdiff.hs
@@ -4,35 +4,47 @@ import Data.List ((\\), intercalate, sort)
 import Haxl.Core
 import qualified Web.Socdiff.Github.DataSource as Github
 import qualified Web.Socdiff.Github.Github as Github
-import System.Directory (doesFileExist, getHomeDirectory)
+import System.Directory (createDirectoryIfMissing, doesFileExist, getHomeDirectory)
 import System.FilePath ((</>))
 
 main :: IO ()
 main = do
-  githubState <- Github.initGlobalState 10
-  ghEnv <- initEnv (stateSet githubState stateEmpty) ()
-  putStrLn "Fetching Github followers"
-  r <- runHaxl ghEnv $ do
-    followers <- Github.getFollowers "CodeBlock" -- TODO: We'll need a config file for this
-    return $ sort $ fmap ("Github:CodeBlock:"++) followers -- TODO: Can we use access the data source's name?
   home <- getHomeDirectory
   let cachePath = home </> ".socdiff_cache"
+  createDirectoryIfMissing False cachePath
+  github cachePath "Github" "CodeBlock"
 
+generateDiff :: String -> [String] -> IO ()
+generateDiff cachePath r = do
   doesCacheExist <- doesFileExist cachePath
   if doesCacheExist
     then do
       oldCache <- fmap lines (readFile cachePath)
-      mapM_ putStrLn (removals oldCache r)
-      mapM_ putStrLn (additions oldCache r)
+      mapM_ putStrLn $ fmap ("- "++) (removals oldCache r)
+      mapM_ putStrLn $ fmap ("+ "++) (additions oldCache r)
     else do
       putStrLn "No previous run detected. Can't generate a diff."
 
-  writeFile cachePath $ intercalate "\n" r
-  appendFile cachePath "\n"
-  putStrLn $ "Stored " ++ cachePath
+github :: String -> String -> String -> IO ()
+github cachePath source username = do
+  let filename = cachePath </> source ++ "_" ++ username
+
+  githubState <- Github.initGlobalState 10
+  ghEnv <- initEnv (stateSet githubState stateEmpty) ()
+  putStrLn $ "Fetching " ++ source ++ " followers"
+
+  r <- runHaxl ghEnv $ do
+    followers <- Github.getFollowers username
+    return $ sort followers
+
+  generateDiff filename r
+
+  writeFile filename $ intercalate "\n" r
+  appendFile filename "\n"
+  putStrLn $ "Stored " ++ filename
 
 removals :: [String] -> [String] -> [String]
-removals old new = fmap ("- "++) (old \\ new)
+removals = (\\)
 
 additions :: [String] -> [String] -> [String]
-additions old new = fmap ("+ "++) (new \\ old)
+additions = flip (\\)
