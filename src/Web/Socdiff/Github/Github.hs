99a4dbc513b9fe8b317447c5c613d3e54002261a
Ricky Elrod <ricky@elrod.me>
2014-06-28 09:00:11 -0400

    expose some library functions for twitter
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Web/Socdiff/Github/Github.hs
+++ src/Web/Socdiff/Github/Github.hs
@@ -1,8 +1,7 @@
 module Web.Socdiff.Github.Github where
 
-import Web.Socdiff.Github.DataSource
-
 import Haxl.Core
+import Web.Socdiff.Github.DataSource
 
 -- | Fetch a list of followers for the given username
 getFollowers :: String -> GenHaxl u [String]
---
+++ src/Web/Socdiff/Twitter/Twitter.hs
@@ -0,0 +1,13 @@
+module Web.Socdiff.Twitter.Twitter where
+
+import qualified Data.Text as T
+import Haxl.Core
+import Web.Socdiff.Twitter.DataSource
+
+-- | Fetch a list of follower *IDs* for the given username
+getFollowers :: String -> GenHaxl u [Integer]
+getFollowers u = dataFetch (GetFollowerIDs u)
+
+-- | Get a list of usernames for the given list of user IDs
+getUsernames :: [Integer] -> GenHaxl u [T.Text]
+getUsernames u = dataFetch (GetUsernames u)
