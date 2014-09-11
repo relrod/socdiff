c6ca42677277fe593522eb5b2b6814c932d8c1c4
Ricky Elrod <ricky@elrod.me>
2014-09-11 00:08:20 -0400

    Add a script to track runs
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ store-via-git.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+socdiff || {
+  echo "Something bad happened. Examine above output and try again."
+  return 1
+}
+
+pushd ~/.socdiff_cache
+[ -d .git ] || git init
+git add -A
+git commit -m '[scripted] socdiff run'
+popd
